3100d97a1c75eff2ad62efad7f73f38cf44edfdf
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-05-02 21:55:10 +0530

    main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,13 @@
+import { assets } from "../../assets/assets";
 import "./Main.css";
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 export default Main;
